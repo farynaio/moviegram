313b484b82485da3b804eb3eb310df1ca0463c68
sanczol <adamfaryna@gmail.com>
2020-10-29 14:38:42 +0000

    Added default API_HOST value

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 export const WEBSITE_NAME = process.env.REACT_APP_WEBSITE_NAME
-export const API_HOST = process.env.REACT_APP_API_HOST
+export const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:3001"
 export const SHOWTIME_FORMAT = "DD/MM/YYYY h:mm:ss a"
